b88bc6d3632a8624512442ee266198e829def0a6
Tim Abbott <tabbott@mit.edu>
2016-07-18 18:37:50 -0700

    setup_disks: Fix postgres version to 9.5.

--- puppet/zulip_ops/files/postgresql/setup_disks.sh
+++ puppet/zulip_ops/files/postgresql/setup_disks.sh
@@ -12,7 +12,7 @@ mkfs.xfs /dev/md0
 echo "/dev/md0   /srv  xfs    noatime,barrier 1 1" >> /etc/fstab
 mount /srv
 
-pg_ctlcluster 9.1 main stop
+pg_ctlcluster 9.5 main stop
 mv /var/lib/postgresql /srv
 ln -s /srv/postgresql/ /var/lib
 
@@ -20,4 +20,4 @@ mv /tmp /srv
 mkdir /tmp
 mount --bind /srv/tmp /tmp
 echo "/srv/tmp   /tmp  bind   defaults,bind 0 0" >> /etc/fstab
-pg_ctlcluster 9.1 main start
+pg_ctlcluster 9.5 main start
