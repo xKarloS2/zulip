f00b5f928dff88825797fcf2fde6285fb29ce44f
Tim Abbott <tabbott@zulipchat.com>
2016-11-21 07:27:09 -0800

    realm_emoji: Use has_request_variables.

--- zerver/views/realm_emoji.py
+++ zerver/views/realm_emoji.py
@@ -1,10 +1,12 @@
+from __future__ import absolute_import
+
 from django.http import HttpRequest, HttpResponse
 from django.core.exceptions import ValidationError
 
-from zerver.models import UserProfile
-
+from zerver.decorator import has_request_variables, REQ, require_realm_admin
 from zerver.lib.response import json_success, json_error
 from zerver.lib.actions import check_add_realm_emoji, do_remove_realm_emoji
+from zerver.models import UserProfile
 
 from six import text_type
 
@@ -12,12 +14,11 @@ def list_emoji(request, user_profile):
     # type: (HttpRequest, UserProfile) -> HttpResponse
     return json_success({'emoji': user_profile.realm.get_emoji()})
 
-def upload_emoji(request, user_profile):
-    # type: (HttpRequest, UserProfile) -> HttpResponse
-    emoji_name = request.POST.get('name', None)
-    emoji_url = request.POST.get('url', None)
+@has_request_variables
+def upload_emoji(request, user_profile, name=REQ(), url=REQ()):
+    # type: (HttpRequest, UserProfile, text_type, text_type) -> HttpResponse
     try:
-        check_add_realm_emoji(user_profile.realm, emoji_name, emoji_url)
+        check_add_realm_emoji(user_profile.realm, name, url)
     except ValidationError as e:
         return json_error(e.messages[0])
     return json_success()
